ea27fbc858e486cde8bf30f8ff75267491e7ca1f
Paul <paul@e-swoosh.co.uk>
2020-05-10 12:40:50 +0100

    just do `$ .\build.bat`

---
+++ build.bat
@@ -0,0 +1,6 @@
+robocopy hook /dist/hook /s /e
+robocopy lib /dist/lib /s /e
+robocopy sounds /dist/sounds /s /e
+copy icon.png dist\icon.png
+copy mod_info.lua dist\mod_info.lua
+copy README.md dist\README.md
