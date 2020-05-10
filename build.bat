c34386ac4224e09121358d09e338f8827da76ab8
Paul <paul@e-swoosh.co.uk>
2020-05-10 14:17:13 +0100

    builds to correct dir, windows backslashes
    backslashes in windoze

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 RMDIR /S /Q %~dp0dist
-mkdir dist
-robocopy hook /dist/hook /s /e
-robocopy lib /dist/lib /s /e
-robocopy sounds /dist/sounds /s /e
+mkdir %~dp0dist
+robocopy %~dp0hook %~dp0dist\hook /s /e
+robocopy %~dp0lib %~dp0dist\lib /s /e
+robocopy %~dp0sounds %~dp0dist\sounds /s /e
 copy icon.png dist\icon.png
 copy mod_info.lua dist\mod_info.lua
 copy README.md dist\README.md
