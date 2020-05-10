a0ed7a283da7c2d2c74891834553b881aede9396
Paul <paul@e-swoosh.co.uk>
2020-05-10 12:41:16 +0100

    remove chat feature, poorly designed feat
    caused our first real bug.
    any player would notify all other players of an upgrade even tho they were not

--- hook/lua/ui/game/chat.lua
+++ hook/lua/ui/game/chat.lua
@@ -23,18 +23,6 @@ do
         if msg.text == 'Starting T2' or msg.text == 'Starting T3' then
             print(bants[rng])
             PlaySound(Sound({Bank = 'upSoundBank', Cue = bantsClips[rng]}))
-
-            LOG('Moving on up: Chatting to alll to say moving on up')
-
-            SessionSendChatMessage({
-                to = 'allies',
-                Chat = true,
-                text = "MOVING ON UP"
-            })
-
-            LOG(
-                'Moving on up: Finished calling moving on up chat msg to oldreceivechat method')
-
             LOG('Moving on up: Forking thread to spam chat')
             asciiMessages.startDisplay()
         end
